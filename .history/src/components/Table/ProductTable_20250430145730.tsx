abfc0ee1af6afd95b88e24935f823b86bd4bde51
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-04-30 15:02:23 +0530

    ui design

---
+++ .history/src/components/Table/ProductTable_20250430145730.tsx
@@ -0,0 +1,288 @@
+// @ts-nocheck
+import { useMemo } from "react";
+import {
+  usePagination,
+  useSortBy,
+  useTable,
+} from "react-table";
+import Loading from "../../ui/Loading";
+import { FcApproval } from "react-icons/fc";
+import {
+  Select,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { FaCaretDown, FaCaretUp } from "react-icons/fa";
+import moment from "moment";
+import { MdDeleteOutline, MdEdit, MdOutlineVisibility } from "react-icons/md";
+import EmptyData from "../../ui/emptyData";
+
+interface EmployeeTableProps {
+  employees: Array<{
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone: string;
+    role: string;
+    isVerified: boolean;
+    createdAt: string;
+    updatedAt: string;
+  }>;
+  isLoadingEmployees: boolean;
+  openUpdateEmployeeDrawerHandler?: (id: string) => void;
+  openEmployeeDetailsDrawerHandler?: (id: string) => void;
+  deleteEmployeeHandler?: (id: string) => void;
+  approveEmployeeHandler?: (id: string) => void;
+}
+
+const EmployeeTable: React.FC<EmployeeTableProps> = ({
+  employees,
+  isLoadingEmployees,
+  openUpdateEmployeeDrawerHandler,
+  openEmployeeDetailsDrawerHandler,
+  deleteEmployeeHandler,
+  approveEmployeeHandler,
+}) => {
+  const columns = useMemo(
+    () => [
+      { Header: "First Name", accessor: "first_name" },
+      { Header: "Last Name", accessor: "last_name" },
+      { Header: "Email", accessor: "email" },
+      { Header: "Phone", accessor: "phone" },
+      { Header: "Role", accessor: "role" },
+      { Header: "isVerified", accessor: "isVerified" },
+      { Header: "Created On", accessor: "createdAt" },
+      { Header: "Last Updated", accessor: "updatedAt" },
+    ],
+    []
+  );
+
+  const verificationStyles = {
+    "not verified": {
+      bg: "#F03E3E",
+      text: "#ffffff",
+    },
+    verified: {
+      bg: "#409503",
+      text: "#ffffff",
+    },
+  };
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    state: { pageIndex },
+    pageCount,
+    setPageSize,
+  } = useTable(
+    {
+      columns,
+      data: employees,
+      initialState: { pageIndex: 0 },
+    },
+    useSortBy,
+    usePagination
+  );
+
+  const dynamicBg = (index) => {
+    return index % 2 !== 0 ? "#ffffff40" : "#ffffff1f";
+  };
+
+  return (
+    <div>
+      {isLoadingEmployees && <Loading />}
+      {employees.length === 0 && !isLoadingEmployees && <EmptyData />}
+
+      {!isLoadingEmployees && employees.length > 0 && (
+        <div>
+          <div className="flex justify-end mb-2">
+            <Select
+              onChange={(e) => setPageSize(Number(e.target.value))}
+              width="80px"
+              color="white"
+              size="sm"
+              borderRadius="md"
+              border="1px solid gray"
+            >
+              <option value={10}>10</option>
+              <option value={20}>20</option>
+              <option value={50}>50</option>
+              <option value={100}>100</option>
+              <option value={100000}>All</option>
+            </Select>
+          </div>
+
+          <TableContainer overflowY="auto" borderRadius="md" className="mx-3 bg-[#ffffff26]">
+            <Table variant="unstyled" {...getTableProps()}>
+              <Thead className="text-sm font-semibold bg-[#ffffff26]">
+                {headerGroups.map((hg) => (
+                  <Tr {...hg.getHeaderGroupProps()} border="none">
+                    {hg.headers.map((column) => (
+                      <Th
+                        border="none"
+                        textTransform="capitalize"
+                        fontSize="14px"
+                        fontWeight="600"
+                        color="white"
+                        {...column.getHeaderProps(column.getSortByToggleProps())}
+                      >
+                        <p className="flex font-[600] text-[15px]">
+                          {column.render("Header")}
+                          {column.isSorted && (
+                            <span>
+                              {column.isSortedDesc ? <FaCaretDown /> : <FaCaretUp />}
+                            </span>
+                          )}
+                        </p>
+                      </Th>
+                    ))}
+                    <Th
+                      textTransform="capitalize"
+                      fontSize="12px"
+                      fontWeight="700"
+                      color="white"
+                      backgroundColor="#ffffff26"
+                      border="none"
+                    >
+                      Actions
+                    </Th>
+                  </Tr>
+                ))}
+              </Thead>
+
+              <Tbody {...getTableBodyProps()}>
+                {page.map((row, index) => {
+                  prepareRow(row);
+                  return (
+                    <Tr
+                      {...row.getRowProps()}
+                      bgColor={dynamicBg(index)}
+                      _hover={{
+                        bg: "#ffffff78",
+                        cursor: "pointer",
+                        transition: "all 0.2s",
+                      }}
+                    >
+                      {row.cells.map((cell) => (
+                        <Td
+                          border="none"
+                          fontWeight="600"
+                          fontSize="sm"
+                          color="gray.300"
+                          {...cell.getCellProps()}
+                        >
+                          {cell.column.id !== "createdAt" &&
+                            cell.column.id !== "updatedAt" &&
+                            cell.column.id !== "isVerified" &&
+                            cell.column.id !== "role" &&
+                            cell.render("Cell")}
+
+                          {cell.column.id === "createdAt" && row.original?.createdAt && (
+                            <span>{moment(row.original.createdAt).format("DD/MM/YYYY")}</span>
+                          )}
+
+                          {cell.column.id === "updatedAt" && row.original?.updatedAt && (
+                            <span>{moment(row.original.updatedAt).format("DD/MM/YYYY")}</span>
+                          )}
+
+                          {cell.column.id === "isVerified" && (
+                            <span
+                              className="rounded-md px-2 py-1"
+                              style={{
+                                backgroundColor: verificationStyles[
+                                  row.original.isVerified ? "verified" : "not verified"
+                                ].bg,
+                                color: verificationStyles[
+                                  row.original.isVerified ? "verified" : "not verified"
+                                ].text,
+                              }}
+                            >
+                              {row.original.isVerified ? "Verified" : "Not Verified"}
+                            </span>
+                          )}
+
+                          {cell.column.id === "role" && (
+                            <span>
+                              {(row.original?.role && row.original.role.role) ||
+                                (row.original.isSuper && "Super Admin") ||
+                                ""}
+                            </span>
+                          )}
+                        </Td>
+                      ))}
+
+                      <Td border="none" className="flex gap-x-2 items-center">
+                        {openEmployeeDetailsDrawerHandler && (
+                          <MdOutlineVisibility
+                            className="hover:scale-110"
+                            size={16}
+                            onClick={() => openEmployeeDetailsDrawerHandler(row.original?._id)}
+                          />
+                        )}
+                        {openUpdateEmployeeDrawerHandler && (
+                          <MdEdit
+                            className="hover:scale-110"
+                            size={16}
+                            onClick={() => openUpdateEmployeeDrawerHandler(row.original?._id)}
+                          />
+                        )}
+                        {deleteEmployeeHandler && (
+                          <MdDeleteOutline
+                            className="hover:scale-110"
+                            size={16}
+                            onClick={() => deleteEmployeeHandler(row.original?._id)}
+                          />
+                        )}
+                        {approveEmployeeHandler && (
+                          <FcApproval
+                            className="hover:scale-110"
+                            size={16}
+                            onClick={() => approveEmployeeHandler(row.original?._id)}
+                          />
+                        )}
+                      </Td>
+                    </Tr>
+                  );
+                })}
+              </Tbody>
+            </Table>
+          </TableContainer>
+
+          <div className="w-[max-content] m-auto my-7">
+            <button
+              className="text-sm mt-2 bg-[#2D3748] py-1 px-4 text-white border-[1px] border-[#2D3748] rounded-3xl disabled:bg-[#b2b2b2] disabled:border-[#b2b2b2] disabled:cursor-not-allowed md:text-lg md:py-1 md:px-4 lg:text-xl lg:py-1 xl:text-base"
+              disabled={!canPreviousPage}
+              onClick={previousPage}
+            >
+              Prev
+            </button>
+            <span className="mx-3 text-sm md:text-lg lg:text-xl xl:text-base">
+              {pageIndex + 1} of {pageCount}
+            </span>
+            <button
+              className="text-sm mt-2 bg-[#2D3748] py-1 px-4 text-white border-[1px] border-[#2D3748] rounded-3xl disabled:bg-[#b2b2b2] disabled:border-[#b2b2b2] disabled:cursor-not-allowed md:text-lg md:py-1 md:px-4 lg:text-xl lg:py-1 xl:text-base"
+              disabled={!canNextPage}
+              onClick={nextPage}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EmployeeTable;
---
+++ .history/src/pages/UserRoles_20250430114530.tsx
@@ -0,0 +1,218 @@
+import { background, Button } from "@chakra-ui/react";
+import { MdOutlineRefresh } from "react-icons/md";
+import { FiSearch } from "react-icons/fi";
+import { useEffect, useState } from "react";
+import { Flex } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  closeAddRoleDrawer,
+  closeRoleDetailsDrawer,
+  closeUpdateRoleDrawer,
+  openAddRoleDrawer,
+  openRoleDetailsDrawer,
+  openUpdateRoleDrawer,
+} from "../redux/reducers/drawersSlice";
+import { toast } from "react-toastify";
+import { useCookies } from "react-cookie";
+import UserRoleTable from "../components/Table/UserRoleTable";
+import AddUserRole from "../components/Drawers/User Role/AddUserRole";
+import UserRoleDetails from "../components/Drawers/User Role/UserRoleDetails";
+import UpdateUserRole from "../components/Drawers/User Role/UpdateUserRole";
+
+const UserRole: React.FC = () => {
+  const { isSuper, allowedroutes } = useSelector((state: any) => state.auth);
+  const isAllowed = isSuper || allowedroutes.includes("user role");
+  const [cookies] = useCookies();
+  const [searchKey, setSearchKey] = useState<string | undefined>();
+  const [roles, setRoles] = useState<any[]>([]);
+  const [filteredRoles, setFilteredRoles] = useState<any[]>([]);
+  const [isLoadingRoles, setIsLoadingRoles] = useState<boolean>(false);
+  const [roleId, setRoleId] = useState<string | undefined>();
+
+  const {
+    isAddRoleDrawerOpened,
+    isUpdateRoleDrawerOpened,
+    isRoleDetailsDrawerOpened,
+  } = useSelector((state: any) => state.drawers);
+  const dispatch = useDispatch();
+
+  const openAddRoleDrawerHandler = () => {
+    dispatch(openAddRoleDrawer());
+  };
+  const closeAddRoleDrawerHandler = () => {
+    dispatch(closeAddRoleDrawer());
+  };
+  const openUpdateRoleDrawerHandler = (id: string) => {
+    setRoleId(id);
+    dispatch(openUpdateRoleDrawer());
+  };
+  const closeUpdateRoleDrawerHandler = () => {
+    dispatch(closeUpdateRoleDrawer());
+  };
+  const openRoleDetailsDrawerHandler = (id: string) => {
+    setRoleId(id);
+    dispatch(openRoleDetailsDrawer());
+  };
+  const closeRoleDetailsDrawerHandler = () => {
+    dispatch(closeRoleDetailsDrawer());
+  };
+
+  const fetchRolesHandler = async () => {
+    try {
+      setIsLoadingRoles(true);
+      const response = await fetch(process.env.REACT_APP_BACKEND_URL + "role/");
+      const data = await response.json();
+      if (!data.success) {
+        throw new Error(data.message);
+      }
+      setRoles(data.roles);
+      setFilteredRoles(data.roles);
+    } catch (error: any) {
+      toast.error(error?.message || "Something went wrong");
+    } finally {
+      setIsLoadingRoles(false);
+    }
+  };
+
+  const deleteRoleHandler = async (id: string) => {
+    try {
+      const response = await fetch(
+        process.env.REACT_APP_BACKEND_URL + "role/",
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${cookies?.access_token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            _id: id,
+          }),
+        }
+      );
+      const data = await response.json();
+      if (!data.success) {
+        throw new Error(data.message);
+      }
+      toast.success(data.message);
+      fetchRolesHandler();
+    } catch (error: any) {
+      toast.error(error?.message || "Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    fetchRolesHandler();
+  }, []);
+
+  useEffect(() => {
+    const searchTxt = searchKey?.toLowerCase();
+    const results = roles.filter(
+      (role: any) =>
+        role.role?.toLowerCase()?.includes(searchTxt) ||
+        role.description?.toLowerCase()?.includes(searchTxt) ||
+        (role?.createdAt &&
+          new Date(role?.createdAt)
+            ?.toISOString()
+            ?.substring(0, 10)
+            ?.split("-")
+            .reverse()
+            .join("")
+            ?.includes(searchTxt?.replaceAll("/", "") || "")) ||
+        (role?.updatedAt &&
+          new Date(role?.updatedAt)
+            ?.toISOString()
+            ?.substring(0, 10)
+            ?.split("-")
+            ?.reverse()
+            ?.join("")
+            ?.includes(searchTxt?.replaceAll("/", "") || ""))
+    );
+    setFilteredRoles(results);
+  }, [searchKey]);
+
+  if (!isAllowed) {
+    return (
+      <div className="text-center text-red-500">
+        You are not allowed to access this route.
+      </div>
+    );
+  }
+
+  return (
+    <div  className="md:pl-3 max-[800px]:pl-0">
+      {/* Add User Role */}
+      {isAddRoleDrawerOpened && (
+        <AddUserRole
+          fetchUserRolesHandler={fetchRolesHandler}
+          closeDrawerHandler={closeAddRoleDrawerHandler}
+        />
+      )}
+      {/* User Role Details */}
+      {isRoleDetailsDrawerOpened && (
+        <UserRoleDetails
+          roleId={roleId}
+          closeDrawerHandler={closeRoleDetailsDrawerHandler}
+        />
+      )}
+      {/* Update User Role */}
+      {isUpdateRoleDrawerOpened && (
+        <UpdateUserRole
+          roleId={roleId}
+          closeDrawerHandler={closeUpdateRoleDrawerHandler}
+          fetchUserRolesHandler={fetchRolesHandler}
+        />
+      )}
+
+<div className="flex flex-col  md:justify-between md:items-center gap-y-2">
+  {/* Title */}
+  <span className="text-2xl md:text-3xl pb-4 text-white  font-bold max-[800px]:text-center">User Roles</span>
+
+  {/* Buttons + Search in one row on mobile */}
+  <div className="flex flex-row  gap-2 w-full md:w-auto mt-2 md:mt-0">
+    {/* Buttons */}
+    <Button
+      onClick={openAddRoleDrawerHandler}
+      bg="#4b87a0d9"
+      color="white"
+      _hover={{ bg: "#4b87a0d9" }}
+    >
+      Add New Role
+    </Button>
+    <Button 
+     onClick={fetchRolesHandler}
+      leftIcon={<MdOutlineRefresh />}
+      variant="outline"
+      color="white"
+      borderColor="white"
+      _hover={{ bg: "#2D3748", color: "white" }}
+    >
+      Refresh
+    </Button>
+
+    {/* Search */}
+    <div className="relative flex-1 min-w-[150px]">
+      <FiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600" />
+      <input
+        className="pl-10 pr-4 py-2 w-full text-sm rounded-[5px] bg-[#ffffff3b] placeholder:text-gray-300 shadow-sm focus:outline-none placeholder:text-gray-500"
+        placeholder="Search roles..."
+        value={searchKey}
+        onChange={(e) => setSearchKey(e.target.value)}
+      />
+    </div>
+  </div>
+</div>
+
+      <div>
+        <UserRoleTable
+          roles={filteredRoles}
+          isLoadingRoles={isLoadingRoles}
+          deleteRoleHandler={deleteRoleHandler}
+          openUpdateRoleDrawerHandler={openUpdateRoleDrawerHandler}
+          openRoleDetailsDrawerHandler={openRoleDetailsDrawerHandler}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UserRole;
